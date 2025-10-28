2ffac6abd36b4b03700cde285f2caddf353ba1f0
isavrodrigues <isabelavr@al.insper.edu.br>
2025-10-28 10:21:41 -0300

    add

---
+++ src/main/java/store/gateway/GatewayResource.java
@@ -0,0 +1,37 @@
+package store.gateway;
+
+import java.net.InetAddress;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GatewayResource {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        String ip = "";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            ip = e.getMessage();
+        }
+        return ResponseEntity.ok()
+            .body(Map.of(
+                "osArch", System.getProperty("os.arch"),
+                "osName", System.getProperty("os.name"),
+                "osVersision", System.getProperty("os.version"),
+                "hostAddress", ip
+            ));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok()
+            .body("API for Store by In5p3R");
+    }
+    
+}
